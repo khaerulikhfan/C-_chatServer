c4cc0cc7794254129dfa60b87fb1b371fb8e4927
waroengponcol <repositori@waroengponcol.xyz>
2016-08-20 12:44:55 +0700

    Commit project pertama

---
+++ chat_server/ChatClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Collections;
+
+namespace chat_server
+{
+    class ChatClient
+    {
+        //—berisi daftar semua client—
+        public static Hashtable AllClients = new Hashtable();
+        //—informasi tentang client—
+        private TcpClient _client;
+        private string _clientIP;
+        private string _clientNick;
+        //—digunakan untuk mengirim/menerima data—
+        private byte[] data;
+        //—apakah nickname sudah dikirim—
+        private bool ReceiveNick = true;
+        public ChatClient(TcpClient client){
+            _client = client;
+            //—mendapatkan client IP address—
+            _clientIP = client.Client.RemoteEndPoint.ToString();
+            //—menambahkan client saat ini ke hash table—
+            AllClients.Add(_clientIP, this);
+            //—memulai membaca data dari client pada
+            // thread terpisah—
+            data = new byte[_client.ReceiveBufferSize];
+            client.GetStream().BeginRead(data, 0,
+            System.Convert.ToInt32(_client.ReceiveBufferSize), ReceiveMessage, null);
+        }
+        public void ReceiveMessage(IAsyncResult ar){
+            //—membaca dari client—
+            int bytesRead;
+            try{
+                lock (_client.GetStream()){
+                    bytesRead = _client.GetStream().EndRead(ar);
+                }
+                //—client tidak tersambung—
+                if (bytesRead < 1){
+                    AllClients.Remove(_clientIP);
+                    Broadcast(_clientNick + " tidak terhubung dengan server");
+                    return;
+                }else{
+                    //—mendapat pesan yang telah dikirim—
+                    string messageReceived = System.Text.Encoding.ASCII.GetString(data, 0, bytesRead);
+                    //—client mengirimkan nicknamenya—
+                    if (ReceiveNick){
+                        _clientNick = messageReceived;
+                        //—memberitahu semua client memasuki
+                        // chat—
+                        Broadcast(_clientNick + " terhubung dengan server");
+                        ReceiveNick = false;
+                    }else{
+                        //—memberikan pesan pada semuanya—
+                        Broadcast(_clientNick + ">" + messageReceived);
+                        }
+                 }
+                 //—melanjutkan membaca dari client—
+                 lock (_client.GetStream()){
+                        _client.GetStream().BeginRead(data, 0,
+                        System.Convert.ToInt32(_client.ReceiveBufferSize),ReceiveMessage,null);
+                 }
+                }
+                catch (Exception ex){
+                    AllClients.Remove(_clientIP);
+                    Broadcast(_clientNick + " tidak terhubung dengan server");
+                }
+        }
+        public void SendMessage(string message){
+            try{
+                //—mengirim text—
+                System.Net.Sockets.NetworkStream ns;
+                lock (_client.GetStream()){
+                    ns = _client.GetStream();
+                }
+             byte[] bytesToSend =
+             System.Text.Encoding.ASCII.GetBytes(message);
+             ns.Write(bytesToSend, 0, bytesToSend.Length);
+             ns.Flush();
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public void Broadcast(string message){
+            //—log secara lokal—
+            Console.WriteLine(message);
+            foreach (DictionaryEntry c in AllClients){
+                //—memberi pesan pada semua user—
+                ((ChatClient)(c.Value)).SendMessage(
+                message + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ chat_server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace chat_server
+{
+    class Program
+    {
+        const int portNo = 500;
+
+        static void Main(string[] args){
+            //==================================================================
+            //Mendapatkan Local Machine’s Host Name & IPAddress
+            //==================================================================
+            String strHostName = "";
+            if (args.Length == 0){
+                // Mendapatkan Ip address dari local machine…
+                // Pertama cari host name dari local machine.
+                strHostName = Dns.GetHostName();
+                Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            }
+            else{
+                strHostName = args[0];
+            }
+            //cari IP berdasarkan HostName
+            IPHostEntry ipEntry = Dns.GetHostByName(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            for (int i = 0; i < addr.Length; i++){
+                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            }
+            //===================================================================
+            System.Net.IPAddress localAdd = System.Net.IPAddress.Parse(addr[0].ToString());
+            TcpListener listener = new TcpListener(localAdd, portNo);
+            listener.Start();
+            while (true)
+            {
+                ChatClient user = new
+                ChatClient(listener.AcceptTcpClient());
+            }
+        }
+    }
+}
